eb0964a6d7e0c0c7fb942ba091b359b074712941
SanjyotZade <zade.sanjyot@gmail.com>
2023-11-10 16:14:52 +0530

    first commit

---
+++ myapp.py
@@ -0,0 +1,46 @@
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import tensorflow as tf
+import numpy as np
+import streamlit as st
+from PIL import Image
+import requests
+from io import BytesIO
+import os
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.title("Location Image Classifier")
+st.text("Provide URL of Location Image for image classification")
+
+current_file_path = os.path.dirname(os.path.abspath(__file__))
+data_path = "data/intel_data/"
+data_path_ = os.path.join(current_file_path, data_path)
+
+@st.cache_data
+def load_model():
+  model = tf.keras.models.load_model(data_path_+'model/')
+  return model
+
+with st.spinner('Loading Model Into Memory....'):
+  model = load_model()
+
+classes = ['buildings', 'forest', 'glacier', 'mountain', 'sea', 'street']
+
+def decode_img(image):
+  img = tf.image.decode_jpeg(image, channels=3)  
+  img = tf.image.resize(img,[150,150])
+  return np.expand_dims(img, axis=0)
+
+path = st.text_input('Enter Image URL to Classify.. ','https://storage.googleapis.com/mlops-bucket-test/test.jpg')
+if path is not None:
+    content = requests.get(path).content
+
+    st.write("Predicted Class :")
+    with st.spinner('classifying.....'):
+      label =np.argmax(model.predict(decode_img(content)),axis=1)
+      st.write(classes[label[0]])    
+    st.write("")
+    image = Image.open(BytesIO(content))
+    st.image(image, caption='Classifying Image', use_column_width=True)
